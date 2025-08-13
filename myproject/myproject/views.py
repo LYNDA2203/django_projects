bc85ba0685d83d82224e50bcb8a7805e431002c3
LYNDA2203 <lindam.rita@gmail.com>
2025-08-13 10:54:21 +0530

    [LYNDA] Feat:Added the new django project

---
+++ myproject/myproject/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def homepage(request):
+    return render(request,'home.html')
+
+def about(request):
+    return render(request,'about.html') 
+
+def register(request):
+    return render(request,'register.html')
