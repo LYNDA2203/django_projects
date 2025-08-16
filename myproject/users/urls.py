8331da50bdf664f15aadf14144d59c239898a63b
LYNDA2203 <lindam.rita@gmail.com>
2025-08-16 17:20:22 +0530

    [LYNDA] Feat:updating the registration page

--- myproject/users/urls.py
+++ myproject/users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
-app_name = 'user'
+app_name = 'users'
 
 urlpatterns = [
-    path('register/',views.register_view,name = "regiser"),
+    path('register/',views.register_view,name = "register"),
+    path('login/',views.login_view,name = "login"),
 ]
